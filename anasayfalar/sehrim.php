972fdce89f40dcd9eceea8b7f330b3aaddb7837d
srrcnkmr <59839171+srrcnkmr@users.noreply.github.com>
2020-05-10 15:37:25 +0300

    Carousel Id changed.

--- anasayfalar/sehrim.php
+++ anasayfalar/sehrim.php
@@ -85,12 +85,12 @@ html;
 </nav>
 
 <header>
-    <div id="carouselExampleIndicators" class="carousel slide slide-ekrani" data-ride="carousel">
+    <div id="sehrimSlide" class="carousel slide slide-ekrani" data-ride="carousel">
         <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#sehrimSlide" data-slide-to="0" class="active"></li>
+            <li data-target="#sehrimSlide" data-slide-to="1"></li>
+            <li data-target="#sehrimSlide" data-slide-to="2"></li>
+            <li data-target="#sehrimSlide" data-slide-to="3"></li>
         </ol>
         <div class="carousel-inne slide-ekrani" role="listbox">
 

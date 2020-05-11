86c9c9385dca26db293980c60a33ecb4daadeb00
srrcnkmr <59839171+srrcnkmr@users.noreply.github.com>
2020-05-11 18:36:46 +0300

    Slide Hatası
    Slide Hatası düzeltidi.

--- anasayfalar/sehrim.php
+++ anasayfalar/sehrim.php
@@ -142,11 +142,11 @@ html;
                 </div>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <a class="carousel-control-prev" href="#sehrimSlide" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <a class="carousel-control-next" href="#sehrimSlide" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
